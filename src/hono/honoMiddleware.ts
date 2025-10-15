3676e1f62a87fb72070709e79055645b1c99a2cd
Torben Köhler <torben@peoplebrandingcompany.com>
2025-10-15 08:42:55 +0200

    add debug mode

--- src/hono/honoMiddleware.ts
+++ src/hono/honoMiddleware.ts
@@ -14,18 +14,25 @@ export const t7mMiddleware = <TEnv extends Env>() =>
 			extras: {
 				includes?: IncludesOf<T>[]
 				wrapper?: (data: OutputOf<T>) => object
+				debug?: boolean
 			} & (PropsOf<T> extends undefined ? { props: never } : { props: PropsOf<T> }),
 			status?: ContentfulStatusCode,
 			headers?: HeaderRecord
 		) {
-			const { props, includes, wrapper } = extras
+			const { props, includes, wrapper, debug } = extras
+			if (debug) console.log('[T7M] Transforming (One): \n', JSON.stringify(input, null, 2).substring(0, 300))
 			const processedIncludes = includes || include?.split(',')
+			if (debug && processedIncludes) console.log('[T7M] Includes Received:', processedIncludes)
 			const transformed = await transformer._transform({
 				input,
 				props,
 				unsafeIncludes: processedIncludes,
 			})
+			if (debug)
+				console.log('[T7M] Transformed (One) ✅: \n', JSON.stringify(transformed, null, 2).substring(0, 300))
 			const response = wrapper ? wrapper(transformed) : transformed
+			if (debug && wrapper)
+				console.log('[T7M] Response (One, Wrapped) ✅: \n', JSON.stringify(response, null, 2).substring(0, 300))
 			// @ts-expect-error Hono's json method has complex overloads that don't align with our return type
 			return c.json(response, status, headers)
 		}
@@ -36,18 +43,28 @@ export const t7mMiddleware = <TEnv extends Env>() =>
 			extras: {
 				includes?: IncludesOf<T>[]
 				wrapper?: (data: OutputOf<T>[]) => object
+				debug?: boolean
 			} & (PropsOf<T> extends undefined ? { props: never } : { props: PropsOf<T> }),
 			status?: ContentfulStatusCode,
 			headers?: HeaderRecord
 		) {
-			const { props, includes, wrapper } = extras
+			const { props, includes, wrapper, debug } = extras
+			if (debug) console.log('[T7M] Transforming many: \n', JSON.stringify(inputs, null, 2).substring(0, 300))
 			const processedIncludes = includes || include?.split(',')
+			if (debug && processedIncludes) console.log('[T7M] Includes Received:', processedIncludes)
 			const transformed = await transformer._transformMany({
 				inputs,
 				props,
 				unsafeIncludes: processedIncludes,
 			})
+			if (debug)
+				console.log('[T7M] Transformed (Many) ✅: \n', JSON.stringify(transformed, null, 2).substring(0, 300))
 			const response = wrapper ? wrapper(transformed) : transformed
+			if (debug && wrapper)
+				console.log(
+					'[T7M] Response (Many, Wrapped) ✅: \n',
+					JSON.stringify(response, null, 2).substring(0, 300)
+				)
 			return c.json(response, status, headers)
 		}
 
